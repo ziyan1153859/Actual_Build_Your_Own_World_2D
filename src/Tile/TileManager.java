f9da56b49924448ea7e0b1dfe7f71459ec50a5bf
Ziyan Batada <ziyan.batada@gmail.com>
2023-05-28 13:31:46 -0700

    adding the player images

--- src/Tile/TileManager.java
+++ src/Tile/TileManager.java
@@ -3,6 +3,7 @@ package Tile;
 import Main.GamePanel;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.IOException;
 
 public class TileManager {
@@ -12,17 +13,26 @@ public class TileManager {
         this.gp = gp;
         tile = new Tile[10];
     }
-    /*
+
     public void getTileImage(){
         try{
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream());
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass.png"));
+
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass.png"));
+
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/Tiles/grass.png"));
         }
         catch(IOException e){
             e.printStackTrace();
         }
     }
+    public void draw(Graphics2D g2){
 
-     */
+    }
+
+
 
 }
